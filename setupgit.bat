b8bbe1e347ddff6d2d88326cb3584faec5ab83b7
CesarCastano <castanc@gmail.com>
2020-07-18 12:12:14 -0300

    first commit

---
+++ setupgit.bat
@@ -0,0 +1,7 @@
+echo "# formio" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/castanc/formio.git
+git push -u origin master
+                
